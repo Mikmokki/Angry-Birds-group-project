7212aef6cc425a5a391cf13d6fbebd04e697352b
Kalle Kankaanpää <unset>
2021-11-18 18:07:18 +0200

    Build parannuksia

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "mainmenu.h"
+/*#include "mainmenu.h"
 #include "mycanvas.h"
 #include "sfml_widget.h"
 
@@ -21,6 +21,43 @@ int main(int argc, char *argv[])
     MyCanvas* SFMLView = new MyCanvas(MainFrame, QPoint(20, 20), QSize(360, 360));
     SFMLView->show();
     */
+/*
+return a.exec();
+}
+*/
 
-    return a.exec();
+#include <SFML\Graphics.hpp>
+#include <box2d\box2d.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Puna";
+    sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "Hello World SFML Window");
+    std::cout << "tiainen" << std::endl;
+    b2Vec2 gravity(0.0f, 9.8f);
+    std::cout << "gravity" << std::endl;
+    b2World *world = new b2World(gravity);
+    std::cout << "maailma" << std::endl;
+    while (sfmlWin.isOpen())
+    {
+
+        sf::Event e;
+        while (sfmlWin.pollEvent(e))
+        {
+
+            switch (e.type)
+            {
+            case sf::Event::EventType::Closed:
+                sfmlWin.close();
+                break;
+            }
+        }
+
+        sfmlWin.clear();
+        //sfmlWin.draw();
+        sfmlWin.display();
+    }
+
+    return 0;
 }
