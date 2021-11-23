f0faeb2ec4f9868d1d63d812d19918358ba7d7b4
Mikko Juusti <mikko.juusti@aalto.fi>
2021-11-23 21:29:09 +0200

    add first slingshot attempt

--- src/main.cpp
+++ src/main.cpp
@@ -29,18 +29,27 @@ return a.exec();
 #include <SFML/Graphics.hpp>
 #include <box2d/box2d.h>
 #include <iostream>
+#include <math.h>
 
 int main()
 {
     sf::RenderWindow sfmlWin(sf::VideoMode(1600, 900), "Angry Birds");
     b2Vec2 gravity(0.0f, 9.8f);
     b2World *world = new b2World(gravity);
-    sf::CircleShape bird;
-    bird.setRadius(30);
-    bird.setOutlineColor(sf::Color::Red);
-    bird.setOutlineThickness(5);
-    bird.setOrigin(30.0f,30.0f);
-    bird.setPosition(100, 600);
+    sf::RectangleShape bird;
+    sf::Texture birdImage;
+    birdImage.loadFromFile("../resources/images/bird.png");
+    bird.setSize(sf::Vector2f(30, 30));
+    bird.setOrigin(15.0f, 15.0f);
+    bird.setPosition(200, 580);
+    bird.setTexture(&birdImage);
+    sf::RectangleShape slingshot(sf::Vector2f(100.0f, 100.0f));
+    sf::Texture slingshotImage;
+    slingshotImage.loadFromFile("../resources/images/slingshot.png");
+    slingshot.setTexture(&slingshotImage);
+    slingshot.setPosition(200, 600);
+    slingshot.setOrigin(50.0f, 50.0f);
+    sf::Vertex line[2] = {};
     while (sfmlWin.isOpen())
     {
 
@@ -54,15 +63,31 @@ int main()
                 sfmlWin.close();
                 break;
             case sf::Event::EventType::Resized:
-                sfmlWin.setSize(sf::Vector2u(e.size.width,e.size.width*9/16));
+                sfmlWin.setSize(sf::Vector2u(e.size.width, e.size.width * 9 / 16));
                 break;
             }
         }
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-           sf::Vector2i pos = sf::Mouse::getPosition(sfmlWin);
-           bird.setPosition(static_cast<float>(pos.x), static_cast<float>(pos.y));
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+
+            // slingshot.setPosition(static_cast<float>(pos.x), static_cast<float>(pos.y));
+        }
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+        {
+            //   bird.setPosition(100, 600);
+        }
+        if (bird.getPosition().y > 0)
+        {
         }
         sfmlWin.clear(sf::Color::Green);
+        sfmlWin.draw(slingshot);
+        sf::Vector2i pos = sf::Mouse::getPosition(sfmlWin);
+        std::cout << pos.x << "  " << pos.y << std::endl;
+        sf::Vertex line[2] =
+            {
+                sf::Vertex(sf::Vector2f(200, 580)),
+                sf::Vertex(sf::Vector2f(static_cast<float>(pos.x), static_cast<float>(pos.y)))};
+        sfmlWin.draw(line, 2, sf::Lines);
         sfmlWin.draw(bird);
         sfmlWin.display();
     }
