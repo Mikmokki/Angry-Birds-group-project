639fc614e8290cb7db40c124b48bf72dd368d5be
Jaakko <jaakko.hhintsala@aalto.fi>
2021-11-25 22:56:13 +0200

    main.cpp updated

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include "level.hpp"
 #include "object.hpp"
+#include "sf_b2_converter.hpp"
+#include <math.h>
 
 int main()
 {
@@ -15,10 +17,21 @@ int main()
     bird.setOutlineThickness(5);
     bird.setOrigin(30.0f, 30.0f);
     bird.setPosition(100, 600);
-    std::string asdf("asdf");
-    Level level(asdf);
-    std::cout << level.GetName();
-    Object object();
+
+    sf::CircleShape bird2;
+    bird2.setRadius(100);
+    bird2.setOutlineColor(sf::Color::Red);
+    bird2.setOutlineThickness(5);
+    bird2.setOrigin(30.0f, 30.0f);
+    bird2.setPosition(100, 600);
+
+    sf::RectangleShape gameArea;
+    gameArea.setSize(sfmlWin.getDefaultView().getSize());
+    gameArea.setFillColor(sf::Color::Magenta);
+    gameArea.setOutlineColor(sf::Color::Black);
+
+    sf::View gameView(sfmlWin.getDefaultView());
+    //sf::FloatRect viewPort(GameView.getViewport());
 
     while (sfmlWin.isOpen())
     {
@@ -26,6 +39,10 @@ int main()
         sf::Event event;
         while (sfmlWin.pollEvent(event))
         {
+            float newRes;
+            float oldRes;
+            auto oldCenter = gameView.getCenter();
+            float gameAreaRes = (gameArea.getSize().x / gameArea.getSize().y);
 
             switch (event.type)
             {
@@ -33,17 +50,114 @@ int main()
                 sfmlWin.close();
                 break;
             case sf::Event::EventType::Resized:
-                sfmlWin.setSize(sf::Vector2u(event.size.width, event.size.width * 9 / 16));
+
+                //sfmlWin.setSize(sf::Vector2u(event.size.width, event.size.width * 9 / 16));
+
+                newRes = (1.f * event.size.width) / (1.f * event.size.height);
+
+                oldRes = (gameView.getSize().x / gameView.getSize().y);
+                //oldRes = (gameArea.getSize().x / gameArea.getSize().y);
+
+                if (newRes > oldRes)
+                {
+                    float k = gameView.getSize().y / (1.f * event.size.height);
+
+                    gameView.setSize(k * event.size.width, k * event.size.height);
+                }
+                else
+                {
+                    float k = gameView.getSize().x / (1.f * event.size.width);
+
+                    gameView.setSize(k * event.size.width, k * event.size.height);
+                }
+
+                if (newRes > gameAreaRes)
+                {
+                    float k = gameArea.getSize().y / (1.f * event.size.height);
+
+                    gameView.setSize(k * event.size.width, k * event.size.height);
+                }
+                else
+                {
+                    float k = gameArea.getSize().x / (1.f * event.size.width);
+
+                    gameView.setSize(k * event.size.width, k * event.size.height);
+                }
+
+                gameView.setCenter(gameArea.getSize() * 0.5f);
+
                 break;
+            case sf::Event::EventType::MouseButtonPressed:
+
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+                {
+                    //GameView.zoom(1.1f);
+
+                    //std::cout << GameView.getViewport().left << " " << GameView.getViewport().top << " "
+                    //<< GameView.getViewport().width << " " << GameView.getViewport().height << std::endl;
+
+                    //sfmlWin.setView(gameView);
+                    break;
+                }
+
+            case sf::Event::EventType::MouseWheelScrolled:
+                /* if (event.mouseWheelScroll.delta > 0 ||
+                    (gameView.getSize().x <= gameArea.getSize().x * (1.f / 1.1f) ||
+                     gameView.getSize().y <= gameArea.getSize().y * (1.f / 1.1f)))
+                {
+                    gameView.zoom(1 - event.mouseWheelScroll.delta * 0.1f);
+                    gameView.move(((sf::Vector2f(bird.getPosition() - sf::Vector2f(gameView.getCenter()))).x) * event.mouseWheelScroll.delta * 0.1f,
+                                  ((sf::Vector2f(bird.getPosition() - sf::Vector2f(gameView.getCenter()))).y) * event.mouseWheelScroll.delta * 0.1f);
+                } */
+
+                gameView.zoom(1 - event.mouseWheelScroll.delta * 0.1f);
+                gameView.move(((sf::Vector2f(bird.getPosition() - sf::Vector2f(gameView.getCenter()))).x) * event.mouseWheelScroll.delta * 0.1f,
+                              ((sf::Vector2f(bird.getPosition() - sf::Vector2f(gameView.getCenter()))).y) * event.mouseWheelScroll.delta * 0.1f);
+
+                std::cout << "wheel movement: " << event.mouseWheelScroll.delta << std::endl;
+
+                break;
+
+                        case sf::Event::EventType::KeyPressed:
+
+                switch (event.key.code)
+                {
+                case sf::Keyboard::Up:
+                    gameView.move(0, -10);
+                    break;
+
+                case sf::Keyboard::Down:
+                    gameView.move(0, 10);
+                    break;
+
+                case sf::Keyboard::Left:
+                    gameView.move(-10, 0);
+                    break;
+
+                case sf::Keyboard::Right:
+                    gameView.move(10, 0);
+                    break;
+
+                default:
+                    break;
+                }
             }
         }
+
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
         {
             sf::Vector2i pos = sf::Mouse::getPosition(sfmlWin);
-            bird.setPosition(static_cast<float>(pos.x), static_cast<float>(pos.y));
+
+            bird.setPosition(sfmlWin.mapPixelToCoords(pos));
         }
+
         sfmlWin.clear(sf::Color::Green);
+
+        sfmlWin.setView(gameView);
+
+        sfmlWin.draw(gameArea);
         sfmlWin.draw(bird);
+        sfmlWin.draw(bird2);
         sfmlWin.display();
     }
 
