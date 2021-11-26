e0e58ef72633e3c4aa4edbbd572c8387b581fd12
Mikko Juusti <mikko.juusti@aalto.fi>
2021-11-26 22:20:27 +0200

    add working slingshot

--- src/main.cpp
+++ src/main.cpp
@@ -36,12 +36,13 @@ int main()
     sf::RenderWindow sfmlWin(sf::VideoMode(1600, 900), "Angry Birds");
     b2Vec2 gravity(0.0f, 9.8f);
     b2World *world = new b2World(gravity);
+    std::pair<double, double> slingMid(200, 580);
     sf::RectangleShape bird;
     sf::Texture birdImage;
     birdImage.loadFromFile("../resources/images/bird.png");
     bird.setSize(sf::Vector2f(30, 30));
     bird.setOrigin(15.0f, 15.0f);
-    bird.setPosition(200, 580);
+    bird.setPosition(slingMid.first, slingMid.second);
     bird.setTexture(&birdImage);
     sf::RectangleShape slingshot(sf::Vector2f(100.0f, 100.0f));
     sf::Texture slingshotImage;
@@ -67,14 +68,10 @@ int main()
                 break;
             }
         }
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-        {
 
-            // slingshot.setPosition(static_cast<float>(pos.x), static_cast<float>(pos.y));
-        }
         if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
         {
-            //   bird.setPosition(100, 600);
+            bird.setPosition(slingMid.first, slingMid.second);
         }
         if (bird.getPosition().y > 0)
         {
@@ -82,12 +79,63 @@ int main()
         sfmlWin.clear(sf::Color::Green);
         sfmlWin.draw(slingshot);
         sf::Vector2i pos = sf::Mouse::getPosition(sfmlWin);
-        std::cout << pos.x << "  " << pos.y << std::endl;
-        sf::Vertex line[2] =
+        //std::cout << pos.x << "  " << pos.y << std::endl;
+        if (slingMid.first - pos.x > 0)
+        {
+
+            double rotation;
+            double direction;
+
+            if (pos.y - slingMid.second > 0)
             {
-                sf::Vertex(sf::Vector2f(200, 580)),
-                sf::Vertex(sf::Vector2f(static_cast<float>(pos.x), static_cast<float>(pos.y)))};
-        sfmlWin.draw(line, 2, sf::Lines);
+                direction = 90 - atan((slingMid.first - pos.x) / (pos.y - slingMid.second)) * 180 / M_PI;
+                rotation = -direction;
+            }
+            else if (pos.y - slingMid.second == 0)
+            {
+                direction = 0;
+                rotation = 0;
+            }
+            else
+            {
+                direction = -90 + atan((slingMid.first - pos.x) / (slingMid.second - pos.y)) * 180 / M_PI;
+                rotation = -direction;
+            };
+            double length = std::min(sqrt(pow(slingMid.first - pos.x, 2) + pow(pos.y - slingMid.second, 2)), 100.0);
+            sf::RectangleShape line(sf::Vector2f(length, 5));
+            sf::RectangleShape line2(sf::Vector2f(length / 3, 4));
+            sf::RectangleShape line3(sf::Vector2f(length / 3, 4));
+
+            line.setPosition(slingMid.first, slingMid.second);
+            line2.setPosition(slingMid.first, slingMid.second);
+            line3.setPosition(slingMid.first, slingMid.second);
+            line.setFillColor(sf::Color(0, 0, 0));
+            line2.setFillColor(sf::Color(0, 0, 0));
+            line3.setFillColor(sf::Color(0, 0, 0));
+            line.rotate(180 + rotation);
+            line2.rotate(150 + rotation);
+            line3.rotate(210 + rotation);
+            sfmlWin.draw(line);
+            sfmlWin.draw(line2);
+            sfmlWin.draw(line3);
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                int time = 0;
+                double gravity = 0.001;
+                while (bird.getPosition().y <= slingMid.second + 200)
+                {
+
+                    sfmlWin.clear(sf::Color::Green);
+                    bird.move(cos((direction)*M_PI / 180) * length / 100, -sin((direction)*M_PI / 180) * length / 100 + time * gravity);
+                    sfmlWin.draw(bird);
+                    sfmlWin.draw(slingshot);
+                    sfmlWin.display();
+                    std::cout << bird.getPosition().x << "  " << bird.getPosition().y << std::endl;
+
+                    time++;
+                }
+            }
+        }
         sfmlWin.draw(bird);
         sfmlWin.display();
     }
