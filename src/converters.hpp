f348589e421ad7be6e7c33330a8fe88fbc0d73a5
Kalle Kankaanpää <kalle.kankaanpaa@aalto.fi>
2021-12-02 14:57:27 +0200

    Feature/slingshot with b2

--- src/converters.hpp
+++ src/converters.hpp
@@ -4,26 +4,26 @@
 #include <SFML/Graphics.hpp>
 #include <box2d/box2d.h>
 #include "math.h"
-// Game and level are imported to get constants defined in those
-#include "level.hpp"
-#include "game.hpp"
+
+const int viewwidth = 1600;
+const int viewheight = 900;
+const int framerate = 60;
+const float time_step = 1.0f / framerate;
+const int velocity_iterations = 6;
+const int position_iterations = 2;
+const b2Vec2 gravity(0.0f, -9.8f);
+const float scale = 100.0f;
 
 namespace utils
 {
-    sf::Vector2f B2ToSfCoords(const b2Vec2 b2_vector)
-    {
-        return sf::Vector2f(scale * b2_vector.x, viewheight - (scale * b2_vector.y));
-    }
+    sf::Vector2f B2ToSfCoords(const b2Vec2 b2_vector);
 
-    b2Vec2 SfToB2Coords(const sf::Vector2f sf_vector)
-    {
-        return b2Vec2(sf_vector.x / scale, (viewheight - sf_vector.y) / scale);
-    }
+    b2Vec2 SfToB2Coords(const sf::Vector2f sf_vector);
 
-    float RadiansToDegrees(const float radians)
-    {
-        return radians * (180.0f * M_PI);
-    }
+    float RadiansToDegrees(const float radians);
+
+    float DegreesToRadians(const float degrees);
+
 }
 
 #endif // ANGRY_BIRDS_CONVERTERS
--- src/game.cpp
+++ src/game.cpp
@@ -1,4 +1,5 @@
 #include "game.hpp"
+#include "math.h"
 
 void Game::LoadLevel(std::string filename)
 {
@@ -15,6 +16,12 @@ void Game::Start()
     sf::RenderWindow window(sf::VideoMode(viewwidth, viewheight), "Angry Birds", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(framerate);
 
+    sf::View game_view(window.getDefaultView());
+
+    bool settled = true; // Is the world in a settled state (nothing is moving)
+    float direction = 0; // Direction of the aiming arrow in degrees
+    float power = 0;     // Power of the aiming arrow (0-100)
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -25,17 +32,68 @@ void Game::Start()
             case sf::Event::EventType::Closed:
                 window.close();
                 break;
+            case sf::Event::EventType::MouseButtonPressed:
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                {
+                    if (settled)
+                    {
+                        //Shoot bird
+                        float x = cos(utils::DegreesToRadians(direction)) * power / 20;
+                        float y = sin(utils::DegreesToRadians(direction)) * power / 20;
+                        current_level_.ThrowBird(0, b2Vec2(x, y));
+                    }
+                }
+                break;
 
+            case sf::Event::EventType::KeyPressed:
+
+                switch (event.key.code)
+                {
+                case sf::Keyboard::Up:
+                    game_view.move(0, -10);
+                    break;
+
+                case sf::Keyboard::Down:
+                    game_view.move(0, 10);
+                    break;
+
+                case sf::Keyboard::Left:
+                    game_view.move(-10, 0);
+                    break;
+
+                case sf::Keyboard::Right:
+                    game_view.move(10, 0);
+                    break;
+
+                default:
+                    break;
+                }
             default:
                 break;
             }
         }
 
-        current_level_.GetWorld()->Step(time_step, velocity_iterations, position_iterations);
-        bool settled = !current_level_.RenderLevel(window);
+        window.clear(sf::Color::White);
+        window.setView(game_view);
+
+        sf::Vector2f bird_position = utils::B2ToSfCoords(current_level_.GetBird()->GetBody()->GetPosition());
+        sf::Vector2f default_center = window.getDefaultView().getCenter();
+
+        // Used std min for the y since sfml coordinates are from top left downwards
+        game_view.setCenter(bird_position.x, std::min(bird_position.y, default_center.y));
+
+        current_level_.GetWorld()
+            ->Step(time_step, velocity_iterations, position_iterations);
+        settled = !current_level_.DrawLevel(window);
+        // Draw the aiming arrow
+        std::tuple<float, float> tuple = current_level_.DrawArrow(window);
+        // Update arrow direction and power
+        direction = std::get<0>(tuple);
+        power = std::get<1>(tuple);
         if (settled)
         {
-            current_level_.ThrowBird(0, b2Vec2(5.0f, 5.0f));
+            current_level_.ResetBird();
         }
+        window.display();
     }
 }
