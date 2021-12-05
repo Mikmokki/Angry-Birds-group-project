961e1f0dde70f6ac5277f9067238064f44806741
Jaakko <jaakko.hhintsala@aalto.fi>
2021-12-05 17:51:10 +0200

    fixed bud

--- src/bird.hpp
+++ src/bird.hpp
@@ -17,7 +17,6 @@ public:
     };
     void MakeSound()
     {
-        sf::SoundBuffer soundBuffer;
 
         if (!soundBuffer.loadFromFile("../../resources/sounds/kukko.wav"))
         {
@@ -25,14 +24,18 @@ public:
             return;
         }
 
-        sf::Sound sound;
         sound.setBuffer(soundBuffer);
+        sound.setVolume(70);
         sound.play();
+
+        std::cout << "tried to make sound " << sound.getStatus() << std::endl;
         return;
     }
 
 private:
     const float bird_scale_ = 0.1f; // Maybe could be replaced by levels scale and bodys radius
+    sf::SoundBuffer soundBuffer;
+    sf::Sound sound;
 };
 
 #endif // ANGRY_BIRDS_BIRD
